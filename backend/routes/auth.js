39a5259014519696f92728e31509c5397a57d420
fabioPuissant <fabio.puissant@hotmail.com>
2020-05-09 18:52:12 +0200

    Nodejs JWT-token auth skeleton

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,62 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("../config/default.json");
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res)=> {
-    res.send('Get logged in user');
+router.get("/", (req, res) => {
+  res.send("Get logged in user");
 });
 
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res)=> {
-    res.send('Log in user');
-});
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email!").isEmail(),
+    check("password", "Please include a password!"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array().map((x) => x.msg) });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      const tokenParams = {
+        expiresIn: 360000,
+      };
+
+      jwt.sign(payload, config.jwtSecret, tokenParams, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (err) {}
+  }
+);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
+const connectDb = require('./config/db');
 const app = express();
 
-const PORT= process.env.PORT || 5000;
+/// Connect db
+connectDb();
 
-app.get('/', (req,res) => {res.send('Hello World')});
+// Init middleware
+app.use(express.json({extended: false}));
 
 // Define Routes
+app.get('/', (req,res) => {res.send('Hello World')});
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 
+const PORT= process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
